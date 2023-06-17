dd93574037387be992d9361d21d9f64de63fba2d
Marko Sakal <marko.sakal.2021@lpnu.ua>
2023-06-17 21:35:07 +0300

    added login & register funct

---
+++ components/js/script.js
@@ -0,0 +1,43 @@
+document.querySelector(".ham").addEventListener("click", function(){
+    document.querySelector(".q").classList.toggle("a");
+    document.querySelector(".w").classList.toggle("b");
+    document.querySelector(".e").classList.toggle("c");
+})
+
+function burgerclick(){
+document.querySelector(".menuContainer").classList.toggle("show");
+}
+
+
+document.getElementById("login-button").addEventListener("click", function() {
+
+    window.location.href = "login.html";
+  });
+  
+  document.getElementById("register-button").addEventListener("click", function() {
+
+    window.location.href = "register.html";
+  });
+
+
+
+const inputs = document.querySelectorAll(".input");
+
+
+function addcl(){
+	let parent = this.parentNode.parentNode;
+	parent.classList.add("focus");
+}
+
+function remcl(){
+	let parent = this.parentNode.parentNode;
+	if(this.value == ""){
+		parent.classList.remove("focus");
+	}
+}
+
+inputs.forEach(input => {
+	input.addEventListener("focus", addcl);
+	input.addEventListener("blur", remcl);
+});
+
